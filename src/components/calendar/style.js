370b23de4d69ad09c0b9d3eca194bc2b57401c8e
joun008 <jouny007@gm.gist.ac.kr>
2022-03-19 01:44:58 +0900

    Calender components만들기

--- src/components/calendar/style.js
+++ src/components/calendar/style.js
@@ -1,8 +1,57 @@
 import { StyleSheet } from 'react-native';
-
-export const styles = StyleSheet.create({
+import { shadow, mediumFontWeight } from '../../style/share';
+const commonCalender = {
+  ...shadow,
+  borderRadius: 10,
+  backgroundColor: 'white',
+  justifyContent: 'Top',
+  alignItems: 'center',
+};
+export const calenderStyles = StyleSheet.create({
   smallCalender: {
     width: 61,
     height: 94,
+    ...commonCalender,
+  },
+  smallMonth: {
+    fontWeight: mediumFontWeight,
+    width: 22,
+    height: 24,
+    fontSize: 18,
+  },
+  smallDay: {
+    fontWeight: mediumFontWeight,
+    width: 44,
+    height: 48,
+    fontSize: 36,
+  },
+  smallDate: {
+    fontWeight: mediumFontWeight,
+    width: 44,
+    height: 18,
+    fontSize: 14,
+  },
+  bigCalender: {
+    width: 75,
+    height: 109,
+    ...commonCalender,
+  },
+  bigMonth: {
+    fontWeight: mediumFontWeight,
+    width: 27,
+    height: 28,
+    fontSize: 24,
+  },
+  bigDay: {
+    fontWeight: mediumFontWeight,
+    width: 54,
+    height: 56,
+    fontSize: 48,
+  },
+  bigDate: {
+    fontWeight: mediumFontWeight,
+    width: 55,
+    height: 21,
+    fontSize: 18,
   },
 });
