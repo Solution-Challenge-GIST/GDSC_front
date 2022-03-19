c91a864105a259966291525384535b6be9bf94fb
MinGook <mgk8609@naver.com>
2022-03-20 00:10:10 +0900

    make responsedisplayWidth

---
+++ src/constants/displayInfo.ts
@@ -0,0 +1,7 @@
+import { Dimensions } from 'react-native';
+
+export const WINDOW_WIDTH = Dimensions.get('window').width;
+export const WINDOW_HEIGHT = Dimensions.get('window').height;
+
+export const FIGMA_WIDTH = 428;
+export const FIGMA_HEIGHT = 926;
