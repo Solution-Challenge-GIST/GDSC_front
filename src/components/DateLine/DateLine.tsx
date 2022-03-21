c840c9f8d2e3cc4e1ade0706069cd77316ba1e33
MinGook <mgk8609@naver.com>
2022-03-21 19:19:29 +0900

    juniorDateLine 컴포넌트를 DateLine 컴포넌트로 변경하고 type을 받아 사용하도록 변경

---
+++ src/components/DateLine/DateLine.tsx
@@ -0,0 +1,39 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Date } from '../../type';
+import { DateLineStyles } from './style';
+
+interface DateWithUserType extends Date {
+  type: 'junior' | 'senior';
+}
+
+const SeperateBar = () => {
+  return <View style={DateLineStyles.juniorSeperateBar} />;
+};
+
+export default function DateLine(props: DateWithUserType) {
+  const { year, month, day, weekday, type } = props;
+  if (type === 'junior') {
+    return (
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <SeperateBar />
+        <Text style={DateLineStyles.juniorText}>
+          {`${year}년 ${month}월 ${day}일 ${weekday}`}
+        </Text>
+        <SeperateBar />
+      </View>
+    );
+  }
+  if (type === 'senior') {
+    return (
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        {/* <SeperateBar /> */}
+        <Text style={DateLineStyles.seniorText}>
+          {`${year}년 ${month}월 ${day}일 ${weekday}`}
+        </Text>
+        {/* <SeperateBar /> */}
+      </View>
+    );
+  }
+  return <></>;
+}
