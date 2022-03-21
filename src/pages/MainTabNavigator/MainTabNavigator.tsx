2d1a472e81ffc07f44f4fdb5e3b8c3860de5b001
MinGook <mgk8609@naver.com>
2022-03-22 00:56:30 +0900

    custom tab bar

--- src/pages/MainTabNavigator/MainTabNavigator.tsx
+++ src/pages/MainTabNavigator/MainTabNavigator.tsx
@@ -3,21 +3,88 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeStackNavigator from './HomeStackNavigator/HomeStackNavigator';
 import AlbumStackNavigator from './AlbumStackNavigator/AlbumStackNavigator';
 import ProfileStackNavigator from './ProfileStackNavigator/ProfileStackNavigator';
+import { baseColor } from '../../style/share';
+import { Image, StyleSheet } from 'react-native';
+import { getDisplayWidth } from '../../utility';
 
 const Tab = createBottomTabNavigator();
 
 export default function MainTabNavigator() {
   return (
     <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={{ headerShown: false }}
+      initialRouteName="HomeStackNavigator"
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: 'absolute',
+          width: '100%',
+          height: 90,
+          backgroundColor: baseColor,
+        },
+      }}
     >
-      <Tab.Screen name="AlbumStackNavigator" component={AlbumStackNavigator} />
-      <Tab.Screen name="HomeStackNavigator" component={HomeStackNavigator} />
+      <Tab.Screen
+        name="AlbumStackNavigator"
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Image
+                style={TabStyles.tabImage}
+                source={require('./MainTabNavigatorImages/albumFocus.png')}
+              />
+            ) : (
+              <Image
+                style={TabStyles.tabImage}
+                source={require('./MainTabNavigatorImages/album.png')}
+              />
+            ),
+        }}
+        component={AlbumStackNavigator}
+      />
+      <Tab.Screen
+        name="HomeStackNavigator"
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Image
+                style={TabStyles.tabImage}
+                source={require('./MainTabNavigatorImages/homeFocus.png')}
+              />
+            ) : (
+              <Image
+                style={TabStyles.tabImage}
+                source={require('./MainTabNavigatorImages/home.png')}
+              />
+            ),
+        }}
+        component={HomeStackNavigator}
+      />
       <Tab.Screen
         name="ProProfileStackNavigatorfile"
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Image
+                style={TabStyles.tabImage}
+                source={require('./MainTabNavigatorImages/profileFocus.png')}
+              />
+            ) : (
+              <Image
+                style={TabStyles.tabImage}
+                source={require('./MainTabNavigatorImages/profile.png')}
+              />
+            ),
+        }}
         component={ProfileStackNavigator}
       />
     </Tab.Navigator>
   );
 }
+
+const TabStyles = StyleSheet.create({
+  tabImage: {
+    width: getDisplayWidth(55),
+    height: getDisplayWidth(55),
+  },
+});
