0f908b21b5fec8d17aabe47ae8753afc60fef58e
joun008 <jouny007@gm.gist.ac.kr>
2022-03-26 23:14:59 +0900

    Calendar 수정

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,28 +1,34 @@
 import { View, Text } from 'react-native';
+import { CalendarDate } from '../../type.js';
 import { calenderStyles } from './style.js';
 
-interface date {
-  month: string;
-  day: string;
-  date: string;
-  fontSize: 'big' | 'small';
-}
-
-export default function Calendar(props: date) {
+export default function Calendar(props: CalendarDate) {
   if (props.fontSize === 'big') {
     return (
       <View style={calenderStyles.bigCalender}>
-        <View style={calenderStyles.bigMonth}>{props.month}</View>
-        <View style={calenderStyles.bigDay}>{props.day}</View>
-        <View style={calenderStyles.bigDate}>{props.date}</View>
+        <View style={calenderStyles.bigMonth}>
+          <Text style={calenderStyles.bigMonthFont}>{props.month}</Text>
+        </View>
+        <View style={calenderStyles.bigDay}>
+          <Text style={calenderStyles.bigDayFont}>{props.day}</Text>
+        </View>
+        <View style={calenderStyles.bigDate}>
+          <Text style={calenderStyles.bigDateFont}>{props.date}</Text>
+        </View>
       </View>
     );
   } else {
     return (
       <View style={calenderStyles.smallCalender}>
-        <View style={calenderStyles.smallMonth}>{props.month}</View>
-        <View style={calenderStyles.smallDay}>{props.day}</View>
-        <View style={calenderStyles.smallDate}>{props.date}</View>
+        <View style={calenderStyles.smallMonth}>
+          <Text style={calenderStyles.smallMonthFont}>{props.month}</Text>
+        </View>
+        <View style={calenderStyles.smallDay}>
+          <Text style={calenderStyles.smallDayFont}>{props.day}</Text>
+        </View>
+        <View style={calenderStyles.smallDate}>
+          <Text style={calenderStyles.smalDatethFont}>{props.date}</Text>
+        </View>
       </View>
     );
   }
