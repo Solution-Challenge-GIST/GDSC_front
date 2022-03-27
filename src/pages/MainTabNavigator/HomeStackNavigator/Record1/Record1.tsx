1e676df3fbb33637fa8ba474ee875ba7daebb0ff
MinGook <mgk8609@naver.com>
2022-03-27 21:45:16 +0900

    레코드 음성 저장되도록 변경/ 이미지,녹음 보내는 api 연동 추가 필요

--- src/pages/MainTabNavigator/HomeStackNavigator/Record1/Record1.tsx
+++ src/pages/MainTabNavigator/HomeStackNavigator/Record1/Record1.tsx
@@ -19,34 +19,35 @@ interface Relationship {
 }
 export default function Record1() {
   const { data, isLoading } = useGetRelations();
-  if (!isLoading) {
-    console.log(data);
-  }
+
   const navigation = useNavigation();
   const goRecord2 = (props: Relationship) => {
     navigation.navigate('Record2', { relationship: props });
   };
-
-  return (
-    <View style={{ flex: 1 }}>
-      <ScrollView bounces={true} style={{ marginBottom: 92 }}>
-        <View style={record1Styles.position}>
-          <Text style={record1Styles.text}>누구에게 음성을 보낼까요?</Text>
-          {data.map((item: Relationship) => {
-            const { junior, relation } = item;
-            return (
-              <View style={{ marginTop: 30 }}>
-                <Relativebutton
-                  key={junior.junior_id}
-                  name={junior.name}
-                  relative={relation}
-                  onPress={() => goRecord2(item)}
-                ></Relativebutton>
-              </View>
-            );
-          })}
-        </View>
-      </ScrollView>
-    </View>
-  );
+  if (!isLoading) {
+    return (
+      <View style={{ flex: 1 }}>
+        <ScrollView bounces={true} style={{ marginBottom: 92 }}>
+          <View style={record1Styles.position}>
+            <Text style={record1Styles.text}>누구에게 음성을 보낼까요?</Text>
+            {data.map((item: Relationship) => {
+              const { junior, relation } = item;
+              return (
+                <View style={{ marginTop: 30 }}>
+                  <Relativebutton
+                    key={junior.junior_id}
+                    name={junior.name}
+                    relative={relation}
+                    onPress={() => goRecord2(item)}
+                  ></Relativebutton>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </View>
+    );
+  } else {
+    return <View></View>;
+  }
 }
--- src/pages/MainTabNavigator/HomeStackNavigator/Record2/Record2.tsx
+++ src/pages/MainTabNavigator/HomeStackNavigator/Record2/Record2.tsx
@@ -4,38 +4,76 @@ import {
   Image,
   View,
   TouchableOpacity,
+  Platform,
 } from 'react-native';
 import React, { useState } from 'react';
 import { record2Styles } from './style';
 import { getDisplayHeight, getDisplayWidth } from '../../../../utility';
 import RadioButton from './radioButton/RadioButton';
 import NormalButton from '../../../../components/button/NormalButton';
+import { Audio } from 'expo-av';
 
-export default function Record2(props) {
-  //props.route.params.person
-  console.log(props.route.params);
-  //아래 결과
-  // Object {
-  //   "person": Object {
-  //     "name": "배정윤",
-  //     "relative": "딸",
-  //   },
-  // }
-  const [isRecoding, setIsRecoding] = useState(false);
-  const [hasRecordFile, sethasRecordFile] = useState(false);
+export default function Record2({ route }) {
+  const relationInfo = route.params.relationship;
+  const [recording, setRecording] = useState<Audio.Recording>();
+  const [recordingStatus, setRecordingStatus] =
+    useState<Audio.RecordingStatus>();
+  const [recordURI, setRecordURI] = useState();
 
-  const toggleRecoding = () => {
-    if (isRecoding) {
-      sethasRecordFile(prev => !prev);
+  const onSave = async () => {
+    let recordFile = new FormData();
+    recordFile.append('file', recordURI);
+    console.log(recordFile);
+  };
+  async function startRecording() {
+    try {
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        playsInSilentModeIOS: true,
+        allowsRecordingIOS: true,
+      }); // <= important for IOS
+
+      const newRrecording = new Audio.Recording();
+      setRecording(newRrecording);
+
+      await newRrecording.prepareToRecordAsync(
+        Audio.RECORDING_OPTIONS_PRESET_LOW_QUALITY,
+      );
+
+      newRrecording.setOnRecordingStatusUpdate(
+        (status: Audio.RecordingStatus) => setRecordingStatus(status),
+      );
+
+      await newRrecording.startAsync();
+    } catch (err) {
+      console.error('Failed to start recording', err);
     }
-    setIsRecoding(prev => !prev);
-  };
+  }
 
-  const onReRecord = () => {
-    setIsRecoding(false);
-    sethasRecordFile(false);
-    //저장된 레코드 파일을 지운는 행동? 추가될 수 있음
-  };
+  async function stopRecording() {
+    if (!recording) {
+      console.log('You are not recording.');
+      return;
+    }
+
+    try {
+      await recording.stopAndUnloadAsync();
+      const URI = recording.getURI();
+      setRecordURI(URI);
+    } catch (error) {
+      // Do nothing -- we are already unloaded.
+    }
+  }
+
+  async function playRecord() {
+    const { sound } = await recording.createNewLoadedSoundAsync();
+    sound.replayAsync();
+  }
+
+  async function reRecord() {
+    setRecording(undefined);
+    setRecordingStatus(undefined);
+  }
 
   return (
     <View>
@@ -46,7 +84,7 @@ export default function Record2(props) {
           marginHorizontal: getDisplayWidth(19),
         }}
       >
-        {!isRecoding && !hasRecordFile && (
+        {!recordingStatus?.isRecording && !recordingStatus?.isDoneRecording && (
           <>
             <Text style={record2Styles.font}>Click button to Record.</Text>
             <Image
@@ -55,7 +93,7 @@ export default function Record2(props) {
             />
           </>
         )}
-        {isRecoding && !hasRecordFile && (
+        {recordingStatus?.isRecording && !recordingStatus?.isDoneRecording && (
           <>
             <Text style={record2Styles.font}>Now Recoding...</Text>
             <Image
@@ -64,7 +102,7 @@ export default function Record2(props) {
             />
           </>
         )}
-        {!isRecoding && hasRecordFile && (
+        {!recordingStatus?.isRecording && recordingStatus?.isDoneRecording && (
           <>
             {/* <Text style={record2Styles.font}>기록이 멈췄어요!</Text> */}
             <Text style={record2Styles.font}>Recorded! Want save?</Text>
@@ -76,26 +114,24 @@ export default function Record2(props) {
         )}
 
         <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-          {!hasRecordFile && !isRecoding && (
+          {!recordingStatus?.isRecording && !recordingStatus?.isDoneRecording && (
             <View style={{ alignItems: 'center' }}>
-              <RadioButton type="record" onPress={toggleRecoding} />
+              <RadioButton type="record" onPress={startRecording} />
               <Text style={record2Styles.buttonText}>Start Recoding</Text>
             </View>
           )}
-          {!hasRecordFile && isRecoding && (
+          {recordingStatus?.isRecording && !recordingStatus?.isDoneRecording && (
             <View style={{ alignItems: 'center' }}>
-              <RadioButton type="stop" onPress={toggleRecoding} />
+              <RadioButton type="stop" onPress={stopRecording} />
               <Text style={record2Styles.buttonText}>Stop Recoding</Text>
             </View>
           )}
         </View>
-        {hasRecordFile && !isRecoding && (
+        {!recordingStatus?.isRecording && recordingStatus?.isDoneRecording && (
           <View style={record2Styles.recordDoneButtonsContainer}>
-            <NormalButton
-              onPress={() => console.log('서버로 음성파일 보내는 함수 실행')}
-              text="Save"
-            />
-            <NormalButton onPress={onReRecord} text="Delete and re-recording" />
+            <NormalButton onPress={onSave} text="Save" />
+            <NormalButton onPress={reRecord} text="Delete and re-recording" />
+            <NormalButton onPress={playRecord} text="Play record" />
           </View>
         )}
       </View>
