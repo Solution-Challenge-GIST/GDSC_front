72d0072b271b65610032ede5a639fb968456ecf3
MinGook <mgk8609@naver.com>
2022-03-30 01:08:48 +0900

    make splash page

--- App.tsx
+++ App.tsx
@@ -6,16 +6,27 @@ import MainTabNavigator from './src/pages/MainTabNavigator/MainTabNavigator';
 import { createStackNavigator } from '@react-navigation/stack';
 import axios from 'axios';
 import { BASE_URL } from './src/constants/urls';
-import { JUNIOR_TOKEN, SENIOR_TOKEN } from './src/constants/token';
+import { SENIOR_TOKEN } from './src/constants/token';
+import * as SplashScreen from 'expo-splash-screen';
 
 const Stack = createStackNavigator();
 export const queryClient = new QueryClient();
-//axios.defaults.headers.common['Authorization'] = JUNIOR_TOKEN;
+// axios.defaults.headers.common['Authorization'] = JUNIOR_TOKEN;
 axios.defaults.headers.common['Authorization'] = SENIOR_TOKEN;
-
 axios.defaults.baseURL = BASE_URL;
 
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function delay_splash() {
+  await SplashScreen.preventAutoHideAsync();
+  await sleep(2500);
+  await SplashScreen.hideAsync();
+}
+
 export default function App() {
+  delay_splash();
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
